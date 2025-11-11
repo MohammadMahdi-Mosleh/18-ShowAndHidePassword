6508bbf6e095a0e14c3d32b145182aac1af82028
mohammad mahdi <moha00ad.mahdi@gmail.com>
2025-11-11 15:44:35 +0330

    create basic HTML layout

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
 "use strict";
-const passwordInput = document.querySelector("input");
-const toggleBtn = document.getElementById("toggleBtn");
-const toggleIcon = document.querySelector(".fa-eye-slash");
-toggleBtn === null || toggleBtn === void 0 ? void 0 : toggleBtn.addEventListener("click", change);
-function change() {
-    const isPassword = (passwordInput === null || passwordInput === void 0 ? void 0 : passwordInput.type) === "password";
-    passwordInput.type = isPassword ? "text" : "password";
-    toggleIcon.classList = isPassword ? "fa-solid fa-eye-slash" : "fa-solid fa-eye";
-}
